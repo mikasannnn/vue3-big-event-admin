17a42a8fc6fd9cb628714d484f1e565034190c9b
mikasannnn <3140297439@qq.com>
2025-08-06 18:49:02 +0800

    VueRouter4

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
 
 
+// 在 Vue3 CompositiAPI 中
+// 1. 获取路由对象 router userouter
+//    const router = useRouter()
+// 2. 获取路由参数 router userouter
+//    const route = useRoute()
+import { useRoute,useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+const goList = () => {
+  router.push('/list')
+  console.log(router,route)
+}
+
 </script>
 
 <template>
-  <div>我是App.vues</div>
+  <div>
+    我是App.vues
+    <button @click="$router.push('/home')">跳转首页</button>
+    <button @click="goList">跳转列表页</button>
+  </div>
+
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// createRouter 创建路由实例
+// 配置 history 模式
+// 1. history模式：createWebHistory      地址栏不加#
+// 2. hash模式：   createWebHashHistory  地址栏加#
+
+// vite 中的环境变量 import.meta.env.BASE_URL 就是 vite.config.js 中的 base配置项 bash: '/',
+
+// vite 的配置 import.meta.env.BASE_URL 是路由的基准地址，默认是 ’/‘
+// https://vitejs.dev/guide/build.html#public-base-path
+
+// 如果将来你部署的域名路径是：http://xxx/my-path/user
+// vite.config.ts  添加配置  base: my-path，路由这就会加上 my-path 前缀了
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
