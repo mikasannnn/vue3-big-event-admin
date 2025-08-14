fe803f0799ff42e325851e5cc231a5ab44cd4129
mikasannnn <3140297439@qq.com>
2025-08-14 17:26:05 +0800

    页`layout`架子[element-plus 菜单]

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores'
 
 // createRouter 创建路由实例
 // 配置 history 模式
@@ -43,6 +44,20 @@ const router = createRouter({
       ]
     }
   ],
+})
+
+// 登录访问拦截 => 默认是直接放行的
+// 根据返回值决定，是放行还是拦截
+// 返回值：
+// 1. undefined / true => 放行
+// 2. false 拦截from的地址页面
+// 3. 具体路径 或 路径对象 拦截到对应的地址
+//     '/login'   { name: 'login' }
+router.beforeEach((to) => {
+  //如果没有token，且访问的是非登录页，拦截到登录，其他情况正常放行
+  const userStore = useUserStore()
+  if (!userStore.token && to.path !== '/login' ) return '/login' // 拦截到登录页
+  //return true // 放行 可以不写 因为默认放行
 })
 
 export default router
